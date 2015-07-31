b8dadac38e98ac4738ab43059ee792705df784f9
ProkopovKirill <prokopov-kirill@yandex.ru>
2015-07-31 19:18:44 +0400

    **** оно конем

---
+++ app/src/main/java/com/example/kirill/myapplication/MainActivity.java
@@ -0,0 +1,195 @@
+package com.example.kirill.myapplication;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final int MENU_RESET_ID = 1;
+    final int MENU_QUIT_ID = 2;
+    // константы для ID пунктов меню
+    final int MENU_ALPHA_ID = 1;
+    final int MENU_SCALE_ID = 2;
+    final int MENU_TRANSLATE_ID = 3;
+    final int MENU_ROTATE_ID = 4;
+    final int MENU_COMBO_ID = 5;
+
+    EditText etNum1;
+    EditText etNum2;
+
+    Button btnAdd;
+    Button btnSub;
+    Button btnMult;
+    Button btnDiv;
+
+    Button btnOpenAct;
+
+    TextView tvResult;
+
+    String oper = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // находим элементы
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etNum2);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+
+        btnOpenAct=(Button) findViewById(R.id.btnOpenAct2);
+
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        // прописываем обработчик
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+
+        btnOpenAct.setOnClickListener(this);
+
+        registerForContextMenu(etNum1);
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.etNum1:
+                // добавляем пункты
+                menu.add(0, MENU_ALPHA_ID, 0, "alpha");
+                menu.add(0, MENU_SCALE_ID, 0, "scale");
+                menu.add(0, MENU_TRANSLATE_ID, 0, "translate");
+                menu.add(0, MENU_ROTATE_ID, 0, "rotate");
+                menu.add(0, MENU_COMBO_ID, 0, "combo");
+                break;
+        }
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Animation anim = null;
+        // определяем какой пункт был нажат
+        switch (item.getItemId()) {
+            case MENU_ALPHA_ID:
+                // создаем объект анимации из файла anim/myalpha
+                anim = AnimationUtils.loadAnimation(this, R.anim.myalpha);
+                break;
+            case MENU_SCALE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myscale);
+                break;
+            case MENU_TRANSLATE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.mytrans);
+                break;
+            case MENU_ROTATE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myrotate);
+                break;
+            case MENU_COMBO_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.mycombo);
+                break;
+        }
+        // запускаем анимацию для компонента tv
+        etNum1.startAnimation(anim);
+        return super.onContextItemSelected(item);
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, MENU_RESET_ID, 0, "Reset");
+        menu.add(0, MENU_QUIT_ID, 0, "Quit");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            case MENU_RESET_ID:
+            // очищаем поля
+                etNum1.setText("");
+                etNum2.setText("");
+                tvResult.setText("");
+                break;
+            case MENU_QUIT_ID:
+            // выход из приложения
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
+
+        // Проверяем поля на пустоту
+        if (TextUtils.isEmpty(etNum1.getText().toString())
+                || TextUtils.isEmpty(etNum2.getText().toString())) {
+            return;
+        }
+
+        // читаем EditText и заполняем переменные числами
+        num1 = Float.parseFloat(etNum1.getText().toString());
+        num2 = Float.parseFloat(etNum2.getText().toString());
+
+        // определяем нажатую кнопку и выполняем соответствующую операцию
+        // в oper пишем операцию, потом будем использовать в выводе
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case R.id.btnSub:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.btnMult:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.btnDiv:
+                oper = "/";
+                result = num1 / num2;
+                break;
+            case R.id.btnOpenAct2:
+                Intent intent=new Intent(this,Main2Activity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
+        // формируем строку вывода
+        tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
+    }
+
+
+}
